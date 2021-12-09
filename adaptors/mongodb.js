0fc35bbbf5b4f2b5b04d35c3b536477176061818
Ali Shahabbasi <shahabbasi.ali.98@gmail.com>
2021-12-09 22:54:45 +0330

    fix: server startup code

--- adaptors/mongodb.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-
-mongoose.connect('mongodb://localhost:27017/test').catch((err) => {
-  console.log('Mongodb connection failed. ERROR::');
-  console.log(err);
-  process.exit(1);
-});

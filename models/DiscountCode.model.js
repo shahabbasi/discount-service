48085e7da3fbcdacde213b28a13ab7c5d5c31641
Ali Shahabbasi <shahabbasi.ali.98@gmail.com>
2021-12-10 13:45:30 +0330

    feat: add discount code model

--- models/DiscountCode.model.js
+++ models/DiscountCode.model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const { model: ArchivedCode } = require('./ArchivedDiscountCode.model');
+const { discountCodeStateEnum } = require('./enums');
 
 
 const discountCodeSchema = new mongoose.Schema({
@@ -13,7 +15,7 @@ const discountCodeSchema = new mongoose.Schema({
     required: false,
     default: 0,
   },
-  activationCounterList: {
+  activationCounterMap: {
     type: mongoose.Schema.Types.Map,
     default: new Map()
   },
@@ -29,10 +31,55 @@ const discountCodeSchema = new mongoose.Schema({
     transform: (doc, ret, options) => {
       delete ret._id;
       return ret
-    }
+    },
+    virtuals: false,
   }
 });
 
+
+discountCodeSchema.method('archive', async function() {
+  const archived = new ArchivedCode(this.toJSON());
+  await archived.save();
+  await this.remove();
+});
+
+discountCodeSchema.method('archiveIfNeeded', async function() {
+  const state = await this.policy.getCodeState(this.usageCounter);
+  if (state === discountCodeStateEnum.EXPIRED) {
+    await this.archive();
+  }
+});
+
+discountCodeSchema.method('previewDiscountAmount', async function(userIdentity, amount) {
+  const policy = this.policy;
+  const result = await policy.getDiscountedAmount(
+    userIdentity,
+    this.usageCounter,
+    this.activationCounterMap,
+    amount
+  );
+  return result;
+});
+
+discountCodeSchema.method('activateForUser', async function(userIdentity, amount) {
+  const policy = this.policy;
+  const discountData = await policy.getDiscountedAmount(
+    userIdentity,
+    this.usageCounter,
+    this.activationCounterMap,
+    amount
+  );
+  this.usageCounter += 1;
+  const userUsage = this.activationCounterMap.get(userIdentity);
+  if (typeof(userUsage) !== 'undefined') {
+    this.activationCounterMap.set(userIdentity, userUsage + 1);
+  } else {
+    this.activationCounterMap.set(userIdentity, 1);
+  }
+  await this.save();
+  await this.archiveIfNeeded();
+  return discountData;
+});
 
 const DiscountCode = new mongoose.model('discountCode', discountCodeSchema);
 
